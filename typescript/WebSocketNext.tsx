4e004a8828421eef7245380ecc85120ef63b987e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-13 14:56:19 +0300

    Change type declaration output directory

---
+++ typescript/WebSocketNext.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import { WebSocketProps } from './types/WebSocket.props'
+
+const WebSocketNext: FC<WebSocketProps> = (props) => {
+  return <div {...props} />
+}
+
+export default WebSocketNext
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,3 +1,4 @@
 import WebSocket from './WebSocket'
+import WebSocketNext from './WebSocketNext'
 
-export { WebSocket }
+export { WebSocket, WebSocketNext }
